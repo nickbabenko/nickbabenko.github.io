7a60cf5ca795ebfc3f144042c317313747775b28
Nick Babenko <nickbabenko@me.com>
2016-02-21 17:31:47 +0000

    Removed un-used

--- GitlabTrello/js/app.js
+++ GitlabTrello/js/app.js
@@ -1,13 +1,4 @@
 TrelloPowerUp.initialize({
-    'attachment-sections': function(t, options) {
-
-    },
-    'card-detail-badges': function(t, options) {
-
-    },
-    'card-buttons': function(t, options) {
-
-    },
     'show-settings': function(t, options) {
         return t.popup({
             title: 'GitLab Settings',
